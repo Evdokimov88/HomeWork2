b2a1e3582ee5d51e17e7a2be77404e006a1751df
Andrey Evdokimov <62350136+Evdokimov88@users.noreply.github.com>
2020-03-25 01:09:06 +0300

    Добвали коммменты

--- src/ru/evdokimov/task2/Goryanoholodno.java
+++ src/ru/evdokimov/task2/Goryanoholodno.java
@@ -5,22 +5,22 @@ import java.util.Scanner;
 public class Goryanoholodno {
     public static void main(String args[]) {
         Random r = new Random(); //создаем рандом r
-        int x = r.nextInt (100); // присваиваем рандомом значение переменной х
-        int stop=505;
-        boolean isTrue = true;
+        int x = r.nextInt (100); // присваиваем рандомом значение переменной х (загадываем число)
+        int stop=505; //стоп слово=)
+        boolean isTrue = true;//булевой переменной присвоил true
         Scanner in = new Scanner (System.in);
         while (isTrue) {
             //цикл будет выполняться, пока значение true
         System.out.println("Игра Горячо-Холодно. Попробуй угадать число от 0 до 100. " +
                 "Введи число, для досрочного прерывания игры введи число 505:");
-        int y = in.nextInt();
-        if (y==stop) break;
+        int y = in.nextInt();//считываем с консоли число
+        if (y==stop) break;// если вдруг надоело играть
                 if ((y<0) | (y>101)) {
-            System.out.println("ведено не корректное число");
+            System.out.println("ведено не корректное число");//ну тут все понятно, нечего вводить не те значения
 
         continue; }
 
-              int z = Math.abs(x-y);
+              int z = Math.abs(x-y); //берем модуль разницы загаданного и введенного с консоли чисел
 
                                        if ((z < 5) & (z > 0)) {
                     System.out.println("Очень Горячо");
@@ -34,9 +34,9 @@ public class Goryanoholodno {
             if (z>25) {
                 System.out.println("Холодно");
                 continue;  }
+//выше условия, при которых приложение подсказывает на сколько близко мы к загадонному числу
 
-
-          isTrue = false;
+          isTrue = false; //присваевам false, когда число угадано
         System.out.println("В точку"); }
     }
 
