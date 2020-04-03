3cbc85ada4bfea4449adbe9fe3e52367e501a016
Andrey Evdokimov <62350136+Evdokimov88@users.noreply.github.com>
2020-04-03 17:24:53 +0300

    Добвали ДЗ4

--- src/ru/evdokimov/task4/Main.java
+++ src/ru/evdokimov/task4/Main.java
@@ -1,12 +1,13 @@
 package ru.evdokimov.task4;
 
-class Hospital {
+class Hospital {       //оздаем класс Больница с полями Имя, город, возраст, диагноз
     String fullname;
     String city;
     int age;
     String diagnosis;
 
-    Hospital(String fullname, String city, int age, String diagnosis) {
+
+    Hospital(String fullname, String city, int age, String diagnosis) { //оздаем конструктор для инициализации полей
         this.fullname = fullname;
         this.city = city;
         this.age = age;
@@ -15,48 +16,53 @@ class Hospital {
 }
 
 
-class InfectionalHosp extends Hospital {
+class InfectionalHosp extends Hospital { //оздаем класс инфекционная больница, который расширяет класс больница
+    //с полями Тип инфекциии и закрытым полем номер палаты(wardNumber)
     String infection;
+    private int wardNumber;
 
     InfectionalHosp(String fullname, String city, int age, String diagnosis, String infection) {
+        //инициализируем поля класса больница и поля класса инфекионная больница
         super(fullname, city, age, diagnosis);
         this.infection = infection;
     }
 
-    public void infectional() {
+    public void infectional() { //оздаем метод inectional и выводим сообщение на консоль
         System.out.println("Лечим инфекционные заболевания");
 
     }
-}
 
-class Traumatology extends Hospital {
-    String trauma;
-
-    Traumatology(String fullname, String city, int age, String diagnosis, String trauma) {
-        super(fullname, city, age, diagnosis);
-        this.trauma = trauma;
+    public void setWardNumber(int ward) { //создаем метом установки номера палаты
+        wardNumber = ward;
     }
 
-    public void gips() {
-
-        System.out.println("При переломе требуется наложить гипс");
+    public int getwardNumber() {//возвращаем номер палаты
+        System.out.println("Пациента с инфекцией необходимо поместить в палату: " + wardNumber);
+        return wardNumber;
     }
 }
 
-class Infectional extends InfectionalHosp {
 
+class Infectional extends InfectionalHosp { //создаем класс бактериальная инфекция, который расширяет класс
+    //инфекционная больница
+    private String antiBiotic;
 
     Infectional(String fullname, String city, int age, String diagnosis, String infection) {
         super(fullname, city, age, diagnosis, infection);
 
     }
 
-    public void antiBiotik() {
-        System.out.println("Лечим бактериальную инфекцию антибиотиками");
+    public void setMassage(String massage) {//метод установки лечения
+
+        antiBiotic = massage;
+    }
+    public String getMassage (){//возвращаем значение
+        System.out.println("Лечим бактериальную инфекцию "+antiBiotic);
+        return antiBiotic;
     }
 }
 
-class SpecialBox extends InfectionalHosp {
+class SpecialBox extends InfectionalHosp {//создаем подкласс в котором лечим противирусными препаратами
     boolean covid19;
 
     SpecialBox(String fullname, String city, int age, String diagnosis, String infection, boolean covid19) {
@@ -69,15 +75,36 @@ class SpecialBox extends InfectionalHosp {
     }
 }
 
+class Traumatology extends Hospital {//класс травматология расширяем класс больница
+    String trauma;
+
+    Traumatology(String fullname, String city, int age, String diagnosis, String trauma) {
+        super(fullname, city, age, diagnosis);
+        this.trauma = trauma;
+    }
+
+    public void gips() {//накладываем гипс
+
+        System.out.println("При переломе требуется наложить гипс");
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-        SpecialBox patientcovid = new SpecialBox("Ivanov Ivan", "Kazan", 47, "CovID19",
-                "Вирусная", true);
-        patientcovid.antiVirus();
-        System.out.println(patientcovid.fullname+patientcovid.diagnosis);
+        SpecialBox patientCovid = new SpecialBox("Ivanov Ivan", "Kazan", 47, "CovID19",
+                "Вирусная", true);//создаем пациента спец бокса с подтвержденным короновирусом=))
+        System.out.println(patientCovid.fullname + patientCovid.diagnosis);
+        patientCovid.antiVirus();//лечим
         Traumatology patientfracture = new Traumatology("Sidorov Sergey", "Chistopol", 35,
-                "Перелом", "Закрытый");
-        patientfracture.gips();
-        System.out.println(patientfracture.fullname+" "+patientfracture.trauma+" "+patientfracture.diagnosis);
+                "Перелом", "Закрытый"); //создаем пациента травматологии
+        System.out.println(patientfracture.fullname + " " + patientfracture.trauma + " " + patientfracture.diagnosis);
+        patientfracture.gips();//лечим
+        Infectional patientInfection = new Infectional("Мешок Пряников", "Казань", 40,
+                "Сальманеллез", "Бактериальная");//создаем пациента бактериального отделения
+        System.out.println(patientInfection.fullname + " " + patientInfection.diagnosis);
+        patientInfection.setWardNumber(15);//назначаем палату
+        patientInfection.getwardNumber();
+        patientInfection.setMassage("антибиотиками");//выписываем лечение
+        patientInfection.getMassage();
     }
 }
