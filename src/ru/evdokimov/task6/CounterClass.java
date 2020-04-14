913968a73255118926bc564ff7e6982d696d6016
Andrey <mail.evdokimov@mail.ru>
2020-04-14 14:56:16 +0300

    ДЗ7-Добавил счетчик объектов

---
+++ src/ru/evdokimov/task6/CounterClass.java
@@ -0,0 +1,19 @@
+package ru.evdokimov.task6;
+
+class CounterClass {
+
+    private static int counter;
+        CounterClass (){
+        counter++;//Конструктор со счетчиком
+    }
+
+    public static int getCounter() {
+        return counter; //создаем статический метод в котором возвращаем значение созданных обьектов
+    }
+    public static void main(String[] args){
+        CounterClass obj =  new CounterClass();
+        CounterClass obj1 = new CounterClass();
+        CounterClass obj2 = new CounterClass();
+        System.out.println("Количество объектов класса CounterClass: "+getCounter());
+    }
+}
