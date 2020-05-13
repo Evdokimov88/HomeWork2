f955255a59790e1e0c8df9845fda4dfd97e82a83
Andrey <mail.evdokimov@yandex.ru>
2020-05-13 21:41:58 +0300

    ДЗ12

--- src/ru/evdokimov/task14/Main.java
+++ src/ru/evdokimov/task14/Main.java
@@ -1,14 +1,13 @@
 package ru.evdokimov.task14;
 
 import java.io.*;
-import java.nio.charset.Charset;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
 
 
-        File file = new File("C:\\test\\products.txt");
+        File file = new File("products.txt");
         Scanner scan = new Scanner(file);
         System.out.printf("%-20s%-16s%-15s%-10s%n","Наименование","Цена","Количество","Стоимость");
         System.out.printf("============================================================%n");
@@ -19,12 +18,11 @@ public class Main {
             float price = Float.parseFloat(s2);
             float quantity = Float.parseFloat(s3);
             float cost = price * quantity;
-
             System.out.printf("%-20s%-7s%-9s%-15s%s%-10.2f%n", s1, s2, "x", s3, "=", cost);
-        }
-
-            {
+            }
+             {
             System.out.printf("============================================================%n");
+                System.out.println();
           //  System.out.printf("Итого:%-55.2f");
            }
 
