103f117502a47631987cafcc54b57c7e8cc3e898
Andrey <mail.evdokimov@yandex.ru>
2020-05-16 20:42:23 +0300

    ДЗ12

--- src/ru/evdokimov/task15/Main.java
+++ src/ru/evdokimov/task15/Main.java
@@ -1,28 +1,36 @@
 package ru.evdokimov.task15;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.*;
 import java.net.URL;
 
+@JsonAutoDetect
+class Foaas{
+    String massage;
+    String subtitle;
 
-class CovidByCountry{
+    Foaas(){
+
+    }
 
 
 }
 public class Main {
     public static void main(String[] args) throws IOException {
 try {
-    CovidByCountry covid;
+    Foaas foaas;
 
-    URL url = new URL("https://api.covid19api.com/");
+    URL url = new URL("https://www.foaas.com/?callback=?");
     InputStream input = url.openStream();
 
     byte[] buffer = input.readAllBytes();
     String str = new String(buffer);
+    //StringReader str = new StringReader(input.toString());
     ObjectMapper mapper = new ObjectMapper();
-    covid = mapper.readValue(str, CovidByCountry.class);
-    System.out.println(covid);
+    foaas = mapper.readValue(str, Foaas.class);
+    System.out.println(foaas);
 }
 catch (IOException e)
 {
