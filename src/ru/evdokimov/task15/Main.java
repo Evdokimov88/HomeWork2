b022192083aa7fd3fcfc3fa76a3a57b907331d5e
Andrey <mail.evdokimov@mail.ru>
2020-05-17 20:13:03 +0300

    ДЗ13

--- src/ru/evdokimov/task15/Main.java
+++ src/ru/evdokimov/task15/Main.java
@@ -2,39 +2,45 @@ package ru.evdokimov.task15;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
+import com.google.gson.annotations.JsonAdapter;
+
 
 import java.io.*;
 import java.net.URL;
 
+import java.util.Date;
+
 @JsonAutoDetect
-class Foaas{
-    String massage;
-    String subtitle;
+class CovidByCountry{
 
-    Foaas(){
-
-    }
-
+String country;
+int confirmed;
+int death;
+int recovered;
+int activity;
+Date date;
 
 }
 public class Main {
-    public static void main(String[] args) throws IOException {
-try {
-    Foaas foaas;
+    public static void main(String[] args) throws IllegalStateException, IOException {
+        try {
+            URL url = new URL("https://api.covid19api.com/live/country/south-africa/status/confirmed");
+            InputStream input = url.openStream();
 
-    URL url = new URL("https://www.foaas.com/?callback=?");
-    InputStream input = url.openStream();
+            byte[] buffer = input.readAllBytes();
+            String str = new String(buffer);
 
-    byte[] buffer = input.readAllBytes();
-    String str = new String(buffer);
-    //StringReader str = new StringReader(input.toString());
+
     ObjectMapper mapper = new ObjectMapper();
-    foaas = mapper.readValue(str, Foaas.class);
-    System.out.println(foaas);
-}
-catch (IOException e)
-{
-    System.out.println(e);
-}
+    CovidByCountry covid = mapper.readValue(str, CovidByCountry.class);
+                //StringReader str = new StringReader(input.toString());
+               /* Gson g = new Gson();
+                CovidByCountry covid = g.fromJson(str, CovidByCountry.class);*/
+                System.out.println(covid);
+
+        } catch (IOException e) {
+            System.out.println(e);
+        }
     }
 }
