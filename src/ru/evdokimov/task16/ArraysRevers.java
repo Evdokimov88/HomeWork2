3684c1e982282884509d8f73b1506071060406b0
Andrey <mail.evdokimov@mail.ru>
2020-05-20 22:59:41 +0300

    ДЗ13

--- src/ru/evdokimov/task16/ArraysRevers.java
+++ src/ru/evdokimov/task16/ArraysRevers.java
@@ -14,14 +14,12 @@ public class ArraysRevers {
 
         }
        System.out.println("Тот же массив в обратном порядке:");
-        for (int i=array.length-1, j=0; i>=array.length/2; i--,j++){
-            int tmp = array[j];
-            array[j]=array[i];
-            array[i]=tmp;
-            System.out.print(array[i]+" ");
+        for (int i=0; i<array.length/2; i++){
+            int tmp = array[i];
+            array[i]=array[array.length-i-1];
+            array[array.length-i-1]=tmp;
+
         }
-      /*  for (int i=array.length-1; i>=0; i--){
-            System.out.print(array[i]+" ");
-        }*/
-    }
+        System.out.print(Arrays.toString(array)+" ");
+       }
 }
--- src/ru/evdokimov/task16/Main.java
+++ src/ru/evdokimov/task16/Main.java
@@ -1,27 +1,26 @@
 package ru.evdokimov.task16;
 
 
+import java.util.Arrays;
+
 public class Main {
-    public static void toLeft(int[][] array) {
+    public static int[][] toLeft(int[][] array) {
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array[i].length-1; j++) {
                 array[i][j] = array[i][j + 1];
 
-
-                System.out.print(" " + array[i][j] + " ");
-
-            }
+           }
+            array[i][array[i].length-1]=0;
             //Я так и не понял, почему у меня изменилас длинна массива, в результате чего я "0" смог записать
             // вместо 10, а не в последнюю ячейку, поэтому схитрил=)))))
-            System.out.println(0);
         }
-
+return array;
 
     }
 
     public static void main(String[] args) {
         int[][] array = {{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}};
-        toLeft(array);
+        System.out.println(Arrays.deepToString(toLeft(array)));
 
     }
 }
