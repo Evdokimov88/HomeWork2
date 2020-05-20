5853a234e5b2b0da2fb377fa3c19629a7c6d0932
Andrey <mail.evdokimov@yandex.ru>
2020-05-20 13:41:51 +0300

    ДЗ16

--- src/ru/evdokimov/task16/Main.java
+++ src/ru/evdokimov/task16/Main.java
@@ -3,22 +3,23 @@ package ru.evdokimov.task16;
 import java.util.Arrays;
 
 public class Main {
-    public static void toLeft(int [][] array,int i, int j1, int j2){
-int tmp = array[i][j1];
-        array[i][j1] = array[i][j2];
-        array[i][j2]=tmp;
-
-    }
-
-    public static void main(String[] args) {
-        int[][] array = {{1,2,3,4,5,6,7,8,9},{1,2,3,4,5,6,7,8,9},{1,2,3,4,5,6,7,8,9}};
+    public static void toLeft(int [][] array){
         for (int i=0; i<3; i++) {
-            for (int j = 1; j < 9; j++) {
-                toLeft(array,i,j,j-1);
+            for (int j =0; j <10; j++) {
+            array[i][j]=array[i][j+1];
+            array[i][9]=0;
+
                 System.out.print(" "+array[i][j]+" ");
             }
             System.out.println();
         }
+
+
+    }
+
+    public static void main(String[] args) {
+        int[][] array = {{1,2,3,4,5,6,7,8,9,10},{1,2,3,4,5,6,7,8,9,10},{1,2,3,4,5,6,7,8,9,10}};
+        toLeft(array);
 
     }
 }
