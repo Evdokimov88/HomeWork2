710ada91f4ad03c23639444d9db7a8ff6bc6504f
Andrey <mail.evdokimov@mail.ru>
2020-05-27 23:12:29 +0300

    ДЗ19

--- src/ru/evdokimov/task19/Main.java
+++ src/ru/evdokimov/task19/Main.java
@@ -8,45 +8,30 @@ class Test {
     }
 
     public Set<String> removeEvenLength1(Set<String> set) {
+        System.out.println("\nЧерез массив:");
         String[] array;
         array = new String[set.size()];
         array = set.toArray(array);
-        System.out.println();
+        String val;
         for (int i = 0; i < array.length; i++) {
-            char[] chars = new char[array[i].length()];
-            for (int j = 0; j < array[i].length(); j++) {
-                chars = array[i].toCharArray();
-            }
+            val = array[i];
+            if (val.length() % 2 == 0) {
 
-            if (chars.length % 2 != 0) {
-                System.out.print(chars);
-                System.out.print(" ");
-
+                set.remove(val);
             }
         }
-
         return set;
     }
+
     public Set<String> removeEvenLength(Set<String> set) {
-
+        System.out.println("\nЧерез итератор");
         Iterator<String> iterator = set.iterator();
-
-        char[] chars = new char[iterator.next().length()];
+        String val;
         while (iterator.hasNext()) {
-            chars = iterator.next().toCharArray();
-
-            if (chars.length % 2 != 0) {
-                System.out.println(String.valueOf(chars));
-            }
+            val = iterator.next();
+            if (val.length() % 2 == 0)
+                iterator.remove();
         }
-        if (chars.length % 2 == 0) {
-            set.remove(String.valueOf(chars));
-
-        }
-
-
-
-
         return set;
     }
 
@@ -69,11 +54,13 @@ public class Main {
         for (String entry : set)
             System.out.print(entry + " ");
         Test test = new Test();
-       // test.removeEvenLength1(set);
-        test.removeEvenLength(set);
+        test.removeEvenLength1(set);
         for (String entry : set)
             System.out.print(entry + " ");
 
+        test.removeEvenLength(set);
+        for (String entry : set)
+            System.out.print(entry + " ");
 
     }
 }
